26b94711d29ba14c2c2cac566a4600b8e2ee2833
LINSEY_HARDWICK <linseyjayne@gmail.com>
2020-02-11 00:08:50 -0600

    simply map out the cat facts

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
 import React from 'react';
 import './App.css';
+import isEmpty from 'lodash/isEmpty';
+
+
+function CatFacts(data) {
+  if (isEmpty(data.facts)) return null;
+  const items = data.facts.map((fact) =>
+    <li key={fact.id}>{fact.fact}</li>);
+  return (
+    <ul>{items}</ul>
+  );
+}
 
 class App extends React.Component {
   render(){
     const { facts, handleCatsClick } = this.props;
-    console.log(facts);
     return (
       <div className="App">
         <header className="App-header">
           cats cats cats
         </header>
-        <p>
-          <br />
+          <div>
           <button onClick={handleCatsClick}>pls give cats</button>
-        </p>
+          <CatFacts facts={facts} />
+          </div>
       </div>
     );
   }
