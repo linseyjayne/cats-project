a3859a4b11b25757998e266fbf0775d57e5a8a3f
LINSEY_HARDWICK <linseyjayne@gmail.com>
2020-02-11 21:53:22 -0600

    clean out a useless comment

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,13 +1,9 @@
-// IMAGE
-// FACT
-
 import React from 'react';
 import './Card.css';
-
 
 export const Card = (fact) => {
     return <div className="cardContainer">
         <div className="box" />
         {fact.fact}
-        </div>
+    </div>
 };
